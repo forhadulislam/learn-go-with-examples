7fa2887f0547b24ffe22d90ecb5ca688125e637c
Forhadul Islam <forhadul.islam@nokia.com>
2019-09-10 11:07:40 +0300

    added all examples

---
+++ examples/httptest.example.go
@@ -0,0 +1,52 @@
+package examples
+
+import (
+	"fmt"
+	"strings"
+	"time"
+	"net/http"
+	"net/http/httptest"
+	"log"
+	"io/ioutil"
+)
+
+func home( w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "%s", "<h2>Welcome to the home Page</h2>" )
+	fmt.Fprintf(w, "%s", "How are you doing today?" )
+}
+
+func work( w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "[ %s", time.Now() )
+	fmt.Fprintf(w, "]")
+}
+
+func main() {
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		if strings.HasPrefix(r.URL.Path, "/configurations/plans") && strings.HasSuffix(r.URL.Path, "/plans") {
+			fmt.Fprintln(w, "Hello plans, client")
+		}
+		if strings.HasPrefix(r.URL.Path, "/configurations/plans") && strings.HasSuffix(r.URL.Path, "/tags") {
+			fmt.Fprintln(w, "I am the tags, client")
+		}
+	}))
+	defer ts.Close()
+
+	//url := ts.URL + "/configurations/plans/sranPlan/tags"
+	url := ts.URL + "/configurations/plans"
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	greeting, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s \n", url)
+	fmt.Printf("%s", greeting)
+
+}
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"bytes"
+	"time"
+	"net/http"
+)
+
+func home( w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "%s", "<h2>Welcome to the home Page</h2>" )
+	fmt.Fprintf(w, "%s", "How are you doing today?" )
+}
+
+func work( w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "[ %s", time.Now() )
+	fmt.Fprintf(w, "]")
+}
+
+func main() {
+	response, _ := http.Get("https://golangcode.com/")
+
+	// The line below would fail because Body = io.ReadCloser
+	// fmt.Printf(response.Body)
+
+	// ...so we convert it to a string by passing it through
+	// a buffer first. A 'costly' but useful process.
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(response.Body)
+	newStr := buf.String()
+	fmt.Printf(newStr)
+
+
+	sPort := "8085"
+	http.HandleFunc("/home", home)
+	http.HandleFunc("/workTime", work)
+	fmt.Println("Server is running at port : " + sPort)
+	http.ListenAndServe(":" + sPort, nil)
+
+}
