b587b2617210bdeebfd385ab6f188676f1853d0d
Forhadul Islam <forhadul.islam@nokia.com>
2019-09-26 14:06:11 +0300

    modified examples

--- examples/01-variables.example.go
+++ examples/01-variables.example.go
@@ -1,13 +1,31 @@
-package example
+package main
 
 import(
 	"fmt"
 )
 
 func main(){
-	var myString string
-	
+	// Defining an empty string
+	var myEmptyString string
 
+	// Defining a string with initial value
+	var myString = "Whaterev"
+
+	// Another way of defining a string with initial value
+	str := "Third string"
+
+	// Initializing a string array of size 5
+	anotherString := make([]string, 5)
+
+
+
+
+	fmt.Println(myEmptyString)
 
 	fmt.Println(myString)
+
+	fmt.Println(str)
+
+	fmt.Println(anotherString)
+	fmt.Println( len(anotherString) )
 }
