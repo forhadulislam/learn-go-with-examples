86eed7b877db20a6e34331cf81d9f23288bda57e
Forhadul Islam <forhadul.islam@nokia.com>
2019-09-30 13:39:27 +0300

    modified examples

--- examples/04-pointers.example.go
+++ examples/04-pointers.example.go
@@ -1,4 +1,4 @@
-package example
+package main
 
 import(
 	"fmt"
@@ -10,5 +10,26 @@ func main(){
 	fmt.Println(s)
 	fmt.Println(pointer)
 	fmt.Println(*pointer)
+
+	aValue := 20
+	bValue := &aValue
+	cV := aValue
+
+	fmt.Println(aValue, bValue, *bValue, cV)
+
+	aValue = 30
+	fmt.Println(aValue, bValue, *bValue, cV)
+
+	*bValue = 50
+	fmt.Println(aValue, bValue, *bValue, cV)
+
+
+	newInt := new(int)
+	fmt.Println(newInt)
+	fmt.Println(*newInt)
+
+	*newInt = 55
+	fmt.Println(newInt)
+	fmt.Println(*newInt)
 
 }
