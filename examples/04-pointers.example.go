345a9910a3342211e14a6bb064ed42d6c82a302b
Forhadul Islam <forhadul.islam@nokia.com>
2019-11-11 22:46:02 +0200

    updated pointer and added stringbuilder example

--- examples/04-pointers.example.go
+++ examples/04-pointers.example.go
@@ -5,7 +5,7 @@ import(
 )
 
 func main(){
-	s := "first text"
+	s := "first string"
 	pointer := &s
 	fmt.Println(s)
 	fmt.Println(pointer)
@@ -15,6 +15,7 @@ func main(){
 	bValue := &aValue
 	cV := aValue
 
+	fmt.Println("// & is memory address and * is value reference")
 	fmt.Println(aValue, bValue, *bValue, cV)
 
 	aValue = 30
@@ -23,11 +24,12 @@ func main(){
 	*bValue = 50
 	fmt.Println(aValue, bValue, *bValue, cV)
 
-
+	fmt.Println("// Creating a new integer")
 	newInt := new(int)
 	fmt.Println(newInt)
 	fmt.Println(*newInt)
 
+	fmt.Println("// *newInt = 55")
 	*newInt = 55
 	fmt.Println(newInt)
 	fmt.Println(*newInt)
