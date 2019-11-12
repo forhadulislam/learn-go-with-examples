f50594da92ebcd84e99a3b43d2893593e50f0b19
Forhadul Islam <forhadul.islam@nokia.com>
2019-11-12 17:26:50 +0200

    updated stringbuilder

--- examples/15-StringBuilder.example.go
+++ examples/15-StringBuilder.example.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-
+var b strings.Builder
+	b.Grow(32)
+	for i, p := range []int{2, 3, 5, 7, 11, 13} {
+		fmt.Fprintf(&b, "%d:%d, ", i+1, p)
+	}
+	s := b.String()   // no copying
+	s = s[:b.Len()-2] // no copying (removes trailing ", ")
+	fmt.Println(s)
 }
