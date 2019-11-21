07cc8987dd01e12b420ea213a703431d4b97ffcd
Forhadul Islam <forhadul.islam@nokia.com>
2019-11-21 15:30:02 +0200

    updated Examples

--- examples/07-error.example.go
+++ examples/07-error.example.go
@@ -1,9 +1,24 @@
 package main
 
-import(
+import (
+	"errors"
 	"fmt"
 )
 
-func main(){
+func fetchData(data string) (string, error){
+	if(data != "invalid"){
+		return "No error", nil
+	}
+	errorData := errors.New("Invalid data received!")
+	return "", errorData
+}
 
+func main(){
+	currentString, err := fetchData("Some data")
+	fmt.Println("Current String :", currentString)
+	fmt.Println("Error :", err)
+
+	currentString, err = fetchData("invalid")
+	fmt.Println("Current String :", currentString)
+	fmt.Println("Error :", err)
 }
