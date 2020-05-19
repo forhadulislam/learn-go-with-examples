b905ffc5bf6a1f42944e62ecb7c764557944311f
Forhadul Islam <forhadul.islam@nokia.com>
2020-05-19 19:07:43 +0300

    updated templates function. added mandatory

--- examples/25-advanced-template.go
+++ examples/25-advanced-template.go
@@ -157,6 +157,12 @@ func main() {
 			return input
 		},
 		"fail": func(msg string) (string, error) { return "", errors.New(msg) },
+		"mandatory": func(i interface{}) (interface{}, error) {
+			if i == nil {
+				return "", errors.New("mandatory value missing")
+			}
+			return i, nil
+		},
 	}
 
 	paths := []string{
