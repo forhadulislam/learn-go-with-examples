170d404498e53a813968d3f1b1b9f0e1e1d6b0ff
Forhadul Islam <forhadul.islam@nokia.com>
2020-05-22 14:59:56 +0300

    updated methods example. added ip validation

--- examples/06-methods.example.go
+++ examples/06-methods.example.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"net"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -16,6 +17,14 @@ func RandomString(length int) string {
 		bytes[i] = characterOptions[rand.Intn(len(characterOptions))]
 	}
 	return string(bytes)
+}
+
+func getRandomIntInRange(min int, max int) int {
+	if max <= 0 || min < 0 || min > max {
+		return 0
+	}
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(max - min + 1) + min
 }
 
 func getFreePort() (int, error) {
@@ -45,10 +54,25 @@ func RandomNumber(length int) string {
 	return string(rand.Intn(100))
 }
 
+func isValidIP(ipAddress string) bool {
+	parsedIP := net.ParseIP(ipAddress)
+	isIPv4 := parsedIP != nil && strings.Contains(ipAddress, ".")
+	isIPv6 := parsedIP != nil && strings.Contains(ipAddress, ":")
+	if isIPv4 || isIPv6 {
+		fmt.Printf("%v is a valid IP address\n", ipAddress)
+		return true
+	}
+
+	fmt.Printf("%v is not a valid IP address\n", ipAddress)
+	return false
+}
+
 func main() {
 	randomString := RandomString(50)
 	fmt.Println("Generated random string : ", randomString)
 	fmt.Println("Generated random number : ", RandomNumber(16))
+
+	fmt.Println("Generated random int in range : ", getRandomIntInRange(1, 3))
 
 	freePort, err := getFreePort()
 	if err != nil {
@@ -56,4 +80,11 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Println("My free port is ", freePort)
+
+	isValidIP("192.168.0.1")
+	isValidIP("FE80:0000:0000:0000:0202:B3FF:FE1E:8329")
+	isValidIP("FE80::0202:B3FF:FE1E:8329")
+	isValidIP("FE80:")
+	isValidIP("684D:1111:222:3333:4444:5555:6:77")
+	isValidIP("http://[2001:db8:0:1]:80")
 }
