b7fb1b67e52999710b55357e010ea37b7d81489c
forhadulislam <shifat2sadi@gmail.com>
2023-10-31 16:40:02 +0200

    add exponential backoff example

---
+++ 35-ExponentialBackOff/example.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"github.com/cenkalti/backoff"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	exponentialBackOff := backoff.NewExponentialBackOff()
+	exponentialBackOff.MaxElapsedTime = 3 * time.Minute
+
+	var value *http.Response
+	var err error
+
+	retryable := func() error {
+		value, err = sendRequest()
+		return err
+	}
+
+	notify := func(err error, t time.Duration) {
+		log.Printf("error: %v happened at time: %v", err, t)
+	}
+
+	err = backoff.RetryNotify(retryable, exponentialBackOff, notify)
+	if err != nil {
+		log.Fatalf("error after retrying: %v", err)
+	}
+
+	fmt.Println(value.StatusCode)
+}
+
+func doSomething() (int64, error) {
+	return 6, nil
+}
+
+func sendRequest() (*http.Response, error) {
+	requestURL := "https://jsonplaceholder.typicode.com/posts/1"
+	req, err := http.NewRequest(http.MethodGet, requestURL, nil)
+	if err != nil {
+		fmt.Printf("client: could not create request: %s\n", err)
+		return &http.Response{}, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{
+		Timeout: 30 * time.Second,
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("client: error making http request: %s\n", err)
+		return &http.Response{}, err
+	}
+
+	// read data from response
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return &http.Response{}, err
+	}
+
+	fmt.Println(string(body))
+
+	return res, nil
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/forhadulislam/learn-go-with-examples
 
 go 1.16
 
-require github.com/tidwall/gjson v1.17.0
+require (
+	github.com/cenkalti/backoff v2.2.1+incompatible
+	github.com/tidwall/gjson v1.17.0
+)
